32494cd37e3a308e6515939dfe8dbf0d57a1e171
rezagama <reza.hidayat@go-jek.com>
2017-03-15 03:34:39 +0700

    [REZA][SIK-9] Menambahkan fungsi edit dan update pada controller arsip

--- app/Http/Controllers/ArsipController.php
+++ app/Http/Controllers/ArsipController.php
@@ -63,11 +63,11 @@ class ArsipController extends Controller
         $arsip->id_arsip = App::generateUniqueID();
         $arsip->no_arsip = $noarsip;
         $arsip->judul = $judul;
-        $arsip->jadwal_retensi = App::dbFormatDate($retensi);
+        $arsip->jadwal_retensi = App::formatDate($retensi);
         $arsip->deskripsi = $deskripsi;
 
         $kategori = Kategori::where('id_kategori', $kategori)->first();
-        $docpath = preg_replace('/\s+/', '', $kategori->nama_kategori);
+        $docpath = preg_replace('/\s+/', '', App::getArchiveRoot($kategori));
         $arsip->status = App::getDocumentStatus($kategori);
 
         $path = 'dokumen/'.strtolower($docpath);
@@ -83,6 +83,64 @@ class ArsipController extends Controller
         }
       } else {
         return Redirect::back()->with('error', 'fail');
+      }
+    } else {
+      return Redirect::back()->with('error', $v->errors()->all()[0]);
+    }
+  }
+
+  public function update(Request $request, $id){
+    $arsip = Arsip::where('id_arsip', $id)->first();
+
+    $judul = $request->input('judul');
+    $noarsip = $request->input('no_arsip');
+    $id_kategori = $request->input('kategori');
+    $retensi = $request->input('jra');
+    $deskripsi = $request->input('deskripsi');
+    $file = $request->file('dokumen');
+
+    $id = $request->input('root');
+    $root = Kategori::where('id_kategori', $id)->first();
+
+    $kategori = Kategori::where('id_kategori', $arsip->id_kategori)->first();
+    $archive = App::getArchiveRootId($kategori);
+
+    if($arsip->no_arsip == $noarsip){
+      $v = Validator::make($request->all(), [
+        'no_arsip' => 'required'
+      ]);
+    } else {
+      $v = Validator::make($request->all(), [
+        'no_arsip' => 'unique:t_arsip'
+      ]);
+    }
+
+    if(!$v->fails()){
+      $arsip->judul = $judul;
+      $arsip->no_arsip = $noarsip;
+      $arsip->deskripsi = $deskripsi;
+      $arsip->jadwal_retensi = App::formatDate($retensi);
+
+      if($archive != $root->id_kategori){
+        $folder = Kategori::where('id_kategori', $id_kategori)->first();
+        $arsip->id_kategori = App::copyArchiveFolder($folder, $root);
+        $arsip->status = $root->tipe_kategori;
+      }
+
+      if(isset($file)){
+        App::hapusFileArsip($arsip);
+        $docpath = preg_replace('/\s+/', '', App::getArchiveRoot($kategori));
+        $path = 'dokumen/'.strtolower($docpath);
+        $filename = App::getFileName($file, $judul, $path);
+        if($file->move($path, $filename)){
+          $arsip->file = $path.'/'.$filename;
+        }
+      }
+
+      if($arsip->save()){
+        return Redirect::back()->with('info', 'Perubahan arsip berhasil disimpan.');
+      } else {
+        return Redirect::back()->with('error', 'Terjadi kesalahan dalam sistem. Harap coba beberapa saat lagi.');
       }
     } else {
       return Redirect::back()->with('error', $v->errors()->all()[0]);
@@ -127,6 +185,33 @@ class ArsipController extends Controller
     $size = App::getPathCount($path);
 
     return view('pages.arsip.detail')->with('arsip', $arsip)
+                ->with('log', $log)
+                ->with('path', $path)
+                ->with('size', $size);
+  }
+
+  public function edit($id){
+    $arsip = DB::table('t_arsip')
+            ->where('id_arsip', $id)
+            ->join('t_akun', 't_arsip.id_user', '=', 't_akun.id_user')
+            ->join('t_kategori', 't_arsip.id_kategori', '=', 't_kategori.id_kategori')
+            ->select('t_arsip.*', 't_akun.nama', 't_akun.id_user', 't_kategori.nama_kategori')
+            ->first();
+    $log = DB::table('t_riwayat')
+            ->where('id_arsip', $id)
+            ->join('t_log', 't_riwayat.id_log', '=', 't_log.id_log')
+            ->join('t_akun', 't_log.id_user', '=', 't_akun.id_user')
+            ->select('t_log.*', 't_akun.foto')
+            ->get();
+    $kategori = Kategori::where('parent', null)->get();
+    $folder = Kategori::where('id_kategori', $arsip->id_kategori)->first();
+    $root = App::getArchiveRootId($folder);
+    $path = App::getArchivePath($folder);
+    $size = App::getPathCount($path);
+
+    return view('pages.arsip.edit')->with('arsip', $arsip)
+                ->with('kategori', $kategori)
+                ->with('root', $root)
                 ->with('log', $log)
                 ->with('path', $path)
                 ->with('size', $size);
