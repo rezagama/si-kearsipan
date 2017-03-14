923dfde99d87fd910361476419063b29525bab0b
rezagama <reza.hidayat@go-jek.com>
2017-03-15 03:35:31 +0700

    [REZA][SIK-9] Menyesuaikan input tanggal

--- public/js/picker.js
+++ public/js/picker.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
+  var datepicker = $('.date').find('input');
+
   $('.datepicker').datepicker({
-   format: 'dd/mm/yyyy'
+    format: 'dd/mm/yyyy'
+  });
+  
+  $(datepicker).change(function(ev){
+    $(datepicker).attr('value', ev.target.value);
   });
 });
