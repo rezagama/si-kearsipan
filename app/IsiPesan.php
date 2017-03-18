31b23aefaa272d67d76d73a4bd1df391dc23b8e1
rezagama <reza.hidayat@go-jek.com>
2017-03-18 20:40:17 +0700

    [REZA][SIK-28] Menambahkan model untuk pesan dan isi pesan

---
+++ app/IsiPesan.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class IsiPesan extends Model
+{
+    protected $table = 't_isi_pesan';
+}
---
+++ app/Pesan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pesan extends Model
+{
+    protected $primaryKey = 'id';
+    protected $table = 't_pesan';
+}
