554cf9cd308a5490b081b9bc40c5913b160bf855
rezagama <reza.hidayat@go-jek.com>
2017-03-20 23:04:24 +0700

    [REZA][SIK-36] Mengimplementasikan pengumuman pada dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,10 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Pengumuman;
+use DB;
 
 class DashboardController extends Controller
 {
     public function index(){
-      return view('pages.dashboard');
+      $pengumuman = DB::table('t_pengumuman')
+                      ->join('t_akun', 't_pengumuman.id_user', '=', 't_akun.id_user')
+                      ->select('t_pengumuman.*', 't_akun.nama', 't_akun.id_user')
+                      ->get();
+
+      $jumlah_pengumuman = Pengumuman::all()->count();
+
+      return view('pages.dashboard')->with('pengumuman', $pengumuman)
+                  ->with('jumlah_pengumuman', $jumlah_pengumuman);
     }
 }
